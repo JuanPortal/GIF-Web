0abd81a54aeaed46965d76842c7ca5df55361505
Portal <juanportalg@gmail.com>
2022-06-20 17:43:17 -0500

    footer fixed

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
     const [images, setImages] = useState([])
+    const {state, loading} = useFetchGifs()
+    console.log(state, loading);
 
     useEffect(() => {
         getGifs(category).then(img => setImages(img))
