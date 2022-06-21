027892e8a0a110c0308269186706abf4a1b1cbb3
Portal <juanportalg@gmail.com>
2022-06-20 19:15:12 -0500

    AddCategory working

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
+
 export const GifApp = () => {
+
+    const [ categories, setCategories ] = useState(['Pokémon'])
+    
     return (
         <>
+            {/* Title */}
             <h1>GIF App</h1>
+
+            {/* Input */}
+            <AddCategory setCategories={setCategories}/>
+
+            {/* List */}
+            <div>
+                {categories.map(category => {
+                    return <div key={category}>{ category }</div>
+                })}
+            </div>
         </>
     )
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+export const AddCategory = ({ setCategories }) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const onInputChange = ({ target }) => setInputValue(target.value)
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        if (inputValue.trim().length < 1) return
+        setCategories(categories => [inputValue, ...categories])
+        setInputValue('')
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type='text'
+                placeholder='Enter a category'
+                value={inputValue}
+                onChange={onInputChange}
+            />
+        </form>
+    )
+}
